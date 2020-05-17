66fb07d9c4bdbf4dffe611e547db0ae144806f13
JBthePenguin <jbthepenguin@netcourrier.com>
2020-05-17 16:40:31 +0000

    ok to import all fake_app tests in testcases.py and make groups

--- testcases.py
+++ testcases.py
@@ -1,3 +1,12 @@
 from fake_app.tests.test_convertor.test_from_str import TestFromStr
+from fake_app.tests.test_convertor.test_from_int import TestFromInt
+from fake_app.tests.test_convertor.test_from_float import TestFromFloat
+from fake_app.tests.test_operator.test_sum_numbers import (
+    TestSumTwo, TestSumThree)
+from fake_app.tests.test_operator.test_mul_numbers import (
+    TestMulTwo, TestMulThree)
 
-groups = [('String', [TestFromStr, ])]
+groups = [
+    ('conv', [TestFromInt, TestFromFloat, TestFromStr]),
+    ('op_two', [TestSumTwo, TestMulTwo]),
+    ('op_three', [TestSumThree, TestMulThree])]
