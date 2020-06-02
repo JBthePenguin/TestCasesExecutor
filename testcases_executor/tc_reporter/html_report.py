cd4f1353f03d7ce22aea1830ca094066934cb02d
JBthePenguin <jbthepenguin@netcourrier.com>
2020-06-02 17:09:11 +0000

    clean html_report.py, next add docstrings in it

--- testcases_executor/tc_reporter/html_report.py
+++ testcases_executor/tc_reporter/html_report.py
@@ -17,47 +17,6 @@ from jinja2 import Environment, PackageLoader
 from testcases_executor.tc_utils import format_duration, BOLD, MUTED, S_RESET
 
 
-def get_icon_color(t_method, result):
-    if t_method in [fail[0] for fail in result.failures]:
-        status_icon, status_color = "thumbs-o-down", "warning"
-        status_name = "FAIL"
-        for test, err in result.failures:
-            if test == t_method:
-                t_error = err
-                break
-    elif t_method in [err[0] for err in result.errors]:
-        status_icon, status_color = "times-circle", "danger"
-        status_name = "ERROR"
-        for test, err in result.errors:
-            if test == t_method:
-                t_error = err
-                break
-    elif t_method in [skp[0] for skp in result.skipped]:
-        status_icon, status_color = "cut", "info"
-        status_name = "SKIP"
-        for test, err in result.skipped:
-            if test == t_method:
-                t_error = err
-                break
-    elif t_method in [e_fail[0] for e_fail in result.expectedFailures]:
-        status_icon, status_color = "stop-circle-o", "danger"
-        status_name = "Expected Fail"
-        for test, err in result.expectedFailures:
-            if test == t_method:
-                t_error = err
-                break
-    else:
-        status_color = 'success'
-        t_error = None
-        if t_method in result.unexpectedSuccesses:
-            status_icon = 'hand-stop-o'
-            status_name = "Unexpected Success"
-        else:
-            status_icon = 'thumbs-o-up'
-            status_name = "SUCCESS"
-    return status_name, status_icon, status_color, t_error
-
-
 class ContextHeader(dict):
     """
     A subclass of dict.
@@ -77,8 +36,8 @@ class ContextHeader(dict):
                 when tests started.
             duration: float
                 duration of all tests in second.
-            n_tests: int
-                number of all tests
+            n_tests: dict
+                number of tests by type (fail, error...).
 
         Keys - Values
         ----------
@@ -90,19 +49,183 @@ class ContextHeader(dict):
                 start time formated.
             duration: str
                 duration formated.
-            n_tests: int
+            n_tests: dict
                 same as parameter.
         """
         super().__init__()
         if status == 'PASSED':
-            status_color = 'success'
+            self['status_color'] = 'success'
         else:
-            status_color = 'danger'
-        self['status'] = status
-        self['status_color'] = status_color
-        self['start_time'] = start_time.strftime("%Y-%m-%d %H:%M:%S")
-        self['duration'] = format_duration(duration)
-        self['n_tests'] = n_tests
+            self['status_color'] = 'danger'
+        self.update({
+            'status': status, 'duration': format_duration(duration),
+            'start_time': start_time.strftime("%Y-%m-%d %H:%M:%S"),
+            'n_tests': n_tests})
+
+
+class ContextGroup(dict):
+    """
+    A subclass of dict.
+
+    Represent a group context with datas needed in template groups.
+    """
+
+    def __init__(self, status, g_name, n_tests, duration):
+        """
+        Init dict, get color depending of status and add necessary keys values.
+
+        Parameters
+        ----------
+            result
+
+        Items
+        ----------
+            (ContextGroup, )
+        """
+        super().__init__()
+        if status == 'PASSED':
+            self['status_color'] = 'success'
+        else:
+            self['status_color'] = 'danger'
+        self.update({
+            'name': g_name, 'status': status, 'n_tests': n_tests,
+            'duration': format_duration(duration)})
+
+
+class ContextMethod(dict):
+
+    def __init__(self, t_method, duration, result):
+        super().__init__()
+        if t_method in [fail[0] for fail in result.failures]:
+            self.update({
+                'status_name': "FAIL", 'status_icon': "thumbs-o-down",
+                'status_color': "warning"})
+            for test, err in result.failures:
+                if test == t_method:
+                    self['error'] = err
+                    break
+        elif t_method in [err[0] for err in result.errors]:
+            self.update({
+                'status_name': "ERROR", 'status_icon': "times-circle",
+                'status_color': "danger"})
+            for test, err in result.errors:
+                if test == t_method:
+                    self['error'] = err
+                    break
+        elif t_method in [skp[0] for skp in result.skipped]:
+            self.update({
+                'status_name': "SKIP", 'status_icon': "cut",
+                'status_color': "info"})
+            for test, err in result.skipped:
+                if test == t_method:
+                    self['error'] = err
+                    break
+        elif t_method in [e_fail[0] for e_fail in result.expectedFailures]:
+            self.update({
+                'status_name': "Expected Fail", 'status_icon': "stop-circle-o",
+                'status_color': "danger"})
+            for test, err in result.expectedFailures:
+                if test == t_method:
+                    self['error'] = err
+                    break
+        else:
+            self.update({'status_color': 'success', 'error': None})
+            if t_method in result.unexpectedSuccesses:
+                self.update({
+                    'status_name': "Unexpected Success",
+                    'status_icon': 'hand-stop-o'})
+            else:
+                self.update({
+                    'status_name': "SUCCESS", 'status_icon': 'thumbs-o-up'})
+        self.update({
+            'name': t_method._testMethodName, 'doc': t_method._testMethodDoc,
+            'duration': format_duration(duration)})
+
+
+class ContextMethods(list):
+    """
+    A subclass of list.
+
+    Represent methods context with datas needed in template testcase.
+    """
+
+    def __init__(self, t_methods, tm_durations, result):
+        super().__init__()
+        for t_method in t_methods:
+            self.append(
+                ContextMethod(t_method, tm_durations[t_method], result))
+
+
+class ContextTestCase(dict):
+    """
+    A subclass of dict.
+
+    Represent a testcase context with datas needed in template testcase.
+    """
+
+    def __init__(self, tc_name, tc_module, duration):
+        super().__init__()
+        self.update({
+            'name': tc_name, 'module': tc_module,
+            'duration': format_duration(duration)})
+
+
+class ContextTestCases(list):
+    """
+    A subclass of list.
+
+    Represent testcases context with datas needed in template groups.
+    """
+
+    def __init__(self, tc_tup, tc_durations, tm_durations, result):
+        """
+        Init dict, get color depending of status and add necessary keys values.
+
+        Parameters
+        ----------
+            result
+
+        Items
+        ----------
+            (ContextGroup, )
+        """
+        super().__init__()
+        for testcase, t_methods in tc_tup:
+            tc_context = ContextTestCase(
+                testcase.__name__, testcase.__module__, tc_durations[testcase])
+            test_methods = ContextMethods(t_methods, tm_durations, result)
+            self.append((tc_context, test_methods))
+
+
+class ContextGroups(list):
+    """
+    A subclass of list.
+
+    Represent context with datas needed in template groups.
+    """
+
+    def __init__(self, result):
+        """
+        Init list, get color depending of status and add necessary keys values.
+
+        Parameters
+        ----------
+            result
+
+        Items
+        ----------
+            (ContextGroup, ContextTestCases)
+        """
+        super().__init__()
+        for group, tc_tup in result.test_methods:
+            group_context = ContextGroup(
+                result.status['groups'][group], group.name,
+                result.n_tests['groups'][group],
+                result.durations['groups'][group])
+            testcases_context = ContextTestCases(
+                tc_tup, result.durations['testcases'],
+                result.durations['tests'], result)
+            self.append((group_context, testcases_context))
 
 
 class TestCasesHtmlReport():
@@ -110,11 +233,6 @@ class TestCasesHtmlReport():
     A class to generate html report.
 
     Use result to get context datas and with a base template construct file.
-
-    Methods
-    ----------
-    get_groups():
-        Called to get context with necessary datas for all groups.
     """
 
     def __init__(self, result):
@@ -143,57 +261,6 @@ class TestCasesHtmlReport():
                 header=ContextHeader(
                     result.status['total'], result.start_time,
                     result.durations['total'], result.n_tests['total']),
-                groups=self.get_groups(result)))
+                groups=ContextGroups(result)))
         result.stream.writeln(
             f"---> {BOLD}{MUTED}{os.path.relpath(report_path)}{S_RESET}\n")
-
-    def get_groups(self, result):
-        """
-        Called to get context with necessary datas for all groups.
-
-        Parameters
-        ----------
-            result: tc_result.TestCasesResult
-                result of tests.
-
-        Return
-        ----------
-            context: dict
-                time, duration, status, number of tests, successes, ...
-        """
-        groups = []
-        for group, g_tests in result.test_methods:
-            # status
-            status = result.status['groups'][group]
-            if status == 'PASSED':
-                status_color = 'success'
-            else:
-                status_color = 'danger'
-            group_dict = {
-                'name': group.name, 'status': status,
-                'status_color': status_color,
-                'n_tests': result.n_tests['groups'][group],
-                'duration': format_duration(result.durations['groups'][group])}
-            tc_methods = []
-            for testcase, t_methods in g_tests:
-                tc_dict = {
-                    'name': testcase.__name__, 'module': testcase.__module__,
-                    'duration': format_duration(
-                        result.durations['testcases'][testcase])}
-                test_methods = []
-                for t_method in t_methods:
-                    status_name, status_icon, status_color, t_error = (
-                        get_icon_color(t_method, result))
-                    t_method_dict = {
-                        'name': t_method._testMethodName,
-                        'duration': format_duration(
-                            result.durations['tests'][t_method]),
-                        'status_name': status_name,
-                        'status_icon': status_icon,
-                        'status_color': status_color,
-                        'error': t_error,
-                        'doc': t_method._testMethodDoc}
-                    test_methods.append(t_method_dict)
-                tc_methods.append((tc_dict, test_methods))
-            groups.append((group_dict, tc_methods))
-        return groups
