fbdd963560ab9fbaa559b5069374129b2ed18728
JBthePenguin <jbthepenguin@netcourrier.com>
2020-06-10 11:36:35 +0000

    add Operating System :: OS Independent in setup

--- setup.py
+++ setup.py
@@ -19,6 +19,7 @@ setuptools.setup(
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GPLv3",
+        "Operating System :: OS Independent",
     ],
     python_requires='>=3.8',
 )
